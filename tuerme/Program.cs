ba0c4a933757c15204893718654021001d9d604e
Yevgen Gugel <148051488+trajil@users.noreply.github.com>
2024-11-08 09:43:49 +0100

    introducing counter

--- tuerme/Program.cs
+++ tuerme/Program.cs
@@ -1,27 +1,30 @@
 ﻿class Tuerme
 {
-    static int Hanoi(int n, int counter, char x, char y, char z)
+    static void AutoSolveHanoi(int n,ref int counter, char x, char y, char z)
     {
         if (n == 1)
         {
+            counter++;
             Console.WriteLine($"Bewege Scheibe 1 von {x} nach {z}");
-            return counter;
+            return;
         }
 
-        Hanoi(n - 1,counter,  x, z, y);
+        AutoSolveHanoi(n - 1,ref counter, x, z, y);
         Console.WriteLine($"Bewege Scheibe {n} von {x} nach {z}");
 
-        Hanoi(n - 1,counter, y, x, z);
-        return 5;
+        counter++;
+        AutoSolveHanoi(n - 1,ref counter, y, x, z);
+
     }
 
     public static void Main()
     {
+
         int n = 3, counter = 0;
-        
+
         Console.WriteLine("Vorgehensweise zum Lösen: ");
-        int m = Convert.ToInt32(Console.ReadLine());
-        counter = Hanoi(m, counter, 'A', 'B', 'C');
+        // int m = Convert.ToInt32(Console.ReadLine());
+        AutoSolveHanoi(n,ref counter, 'A', 'B', 'C');
         Console.WriteLine($"In {counter} Zügen.");
     }
 }
