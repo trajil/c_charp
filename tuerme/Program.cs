3e446cbebad725894b5eb10a2217d52f66d2e0b2
Yevgen Gugel <148051488+trajil@users.noreply.github.com>
2024-11-08 10:59:25 +0100

    introducing replay function and custom disk setting

--- tuerme/Program.cs
+++ tuerme/Program.cs
@@ -1,30 +1,102 @@
 ﻿class Tuerme
 {
-    static void AutoSolveHanoi(int n,ref int counter, char x, char y, char z)
+    static void AutoSolveHanoi(int n, ref int counter, char x, char y, char z)
     {
         if (n == 1)
         {
             counter++;
-            Console.WriteLine($"Bewege Scheibe 1 von {x} nach {z}");
+            Console.WriteLine($"Moving disk 1 from {x} to {z}");
             return;
         }
 
-        AutoSolveHanoi(n - 1,ref counter, x, z, y);
-        Console.WriteLine($"Bewege Scheibe {n} von {x} nach {z}");
+        AutoSolveHanoi(n - 1, ref counter, x, z, y);
+        Console.WriteLine($"Moving disk {n} from {x} to {z}");
 
         counter++;
-        AutoSolveHanoi(n - 1,ref counter, y, x, z);
+        AutoSolveHanoi(n - 1, ref counter, y, x, z);
 
     }
-
-    public static void Main()
+    static void ChooseMethod()
     {
 
-        int n = 3, counter = 0;
+    }
+    static int SetAmountOfDisks(int n)
+    {
+        int diskLimiter = 8;
+        bool correctAmountOfDisks = false;
 
-        Console.WriteLine("Vorgehensweise zum Lösen: ");
-        // int m = Convert.ToInt32(Console.ReadLine());
-        AutoSolveHanoi(n,ref counter, 'A', 'B', 'C');
-        Console.WriteLine($"In {counter} Zügen.");
+        do
+        {
+            Console.WriteLine($"Please enter the number of disks to be used (between 1 and {diskLimiter}): ");
+
+            // forcing integer input from user
+            bool notAnInt = true;
+            string input = "";
+            while (notAnInt)
+            {
+                input = Console.ReadLine();
+                try
+                {
+                    n = Convert.ToInt32(input);
+                    notAnInt = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+            }
+
+            if (n >= 1 && n <= diskLimiter)
+                correctAmountOfDisks = true;
+            else if (n < 1)
+            {
+                Console.WriteLine("You entered too few disks, it must be over 1");
+
+            }
+            else
+            {
+                Console.WriteLine($"You entered too many disks, it must be under {diskLimiter + 1}");
+            }
+        } while (!correctAmountOfDisks);
+
+        return n;
+    }
+
+    public static bool AskPlayerForReplay(bool playerWantsToContinue)
+    {
+
+        Console.Write("Do you want to continue? (Y/N): ");
+        char input = Console.ReadKey().KeyChar;
+
+        if (input == 'Y' || input == 'y')
+        {
+            Console.WriteLine("\nYou chose to continue.");
+            return playerWantsToContinue = true;
+        }
+        else if (input == 'N' || input == 'n')
+        {
+            Console.WriteLine("\nYou chose to exit.");
+            return playerWantsToContinue = false;
+        }
+        else
+        {
+            Console.WriteLine("\nInvalid choice.");
+            return playerWantsToContinue = false;
+        }
+    }
+    public static void Main()
+    {
+        bool playerWantsToContinue = false;
+
+        do
+        {
+            int n = 1, counter = 0;
+
+            n = SetAmountOfDisks(n);
+            Console.WriteLine("Path to completing the towers: ");
+            AutoSolveHanoi(n, ref counter, 'A', 'B', 'C');
+            Console.WriteLine($"Amout of turns needed: {counter}");
+            playerWantsToContinue = AskPlayerForReplay(playerWantsToContinue);
+        } while (playerWantsToContinue);
     }
 }
